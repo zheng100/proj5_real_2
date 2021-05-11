dce64d9b56886c90f1dfdd013e3ba8f1c04d6c2c
zheng100 <33104893+zheng100@users.noreply.github.com>
2021-05-11 11:30:10 -0700

    beautified code

--- proj5/models/pipelines/top5_reorder_category.sql
+++ proj5/models/pipelines/top5_reorder_category.sql
@@ -1,6 +1,6 @@
 select b.department_id, AVG(reorder_pct) as cat_reorder_avg
 from {{ ref('product_reorder') }} a, {{source('insta_data_set','products')}} b
-WHERE a.product_id = b.product_id 
+WHERE a.product_id = b.product_id
 GROUP BY b.department_id
 ORDER BY cat_reorder_avg DESC
 LIMIT 5
